27544f1c222a77801aaef25594b88b334a15e76e
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-10-27 16:42:47 +0200

    [SC-225] Standardize licensing (#9)

--- src/testing/ArbitrumDomain.sol
+++ src/testing/ArbitrumDomain.sol
@@ -1,18 +1,4 @@
-// SPDX-FileCopyrightText: © 2022 Dai Foundation <www.daifoundation.org>
 // SPDX-License-Identifier: AGPL-3.0-or-later
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 pragma solidity >=0.8.0;
 
 import { Vm } from "forge-std/Vm.sol";
@@ -108,7 +94,7 @@ contract ArbitrumDomain is BridgedDomain {
             deployed := create(0, add(bytecode, 0x20), mload(bytecode))
         }
         vm.etch(ARB_SYS, deployed.code);
-        
+
         _hostDomain.selectFork();
     }
 
@@ -173,5 +159,5 @@ contract ArbitrumDomain is BridgedDomain {
             selectFork();
         }
     }
-    
+
 }
